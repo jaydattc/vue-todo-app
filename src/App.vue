9c4041fd19b717a1fc92cb2485fa8655cf11d386
Jaydattsinh Champavat <jaydattc@gmail.com>
2019-03-31 17:04:00 +0530

    source commit-1

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,160 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div id="app" class="center">
+    <div class="card">
+      <div class="header center">
+        <h2>ToDo App</h2>
+      </div>
+
+      <input
+        v-model="newTodo"
+        @keydown.enter="addTodo"
+        type="text"
+        class="input-text"
+        placeholder="add todo item..."
+      />
+
+      <ul>
+        <todoList
+          v-for="todo in todos"
+          :key="todo.id"
+          :todo="todo"
+          @done="completed"
+        />
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
+import todoList from "./components/todoList";
+let idinit = 0;
 export default {
   name: "app",
   components: {
-    HelloWorld
+    todoList
+  },
+  data() {
+    return {
+      newTodo: "",
+      editMode: false,
+      todos: [
+        {
+          id: idinit++,
+          text: "something",
+          done: false
+        },
+        {
+          id: idinit++,
+          text: "something more",
+          done: false
+        }
+      ]
+    };
+  },
+  methods: {
+    // editTodo: function() {
+    //   this.todos = this.todos.map( todo => {
+
+    //   })
+    // },
+
+    addTodo: function() {
+      var trimmed = this.newTodo.trim();
+      if (trimmed)
+        this.todos.push({ id: idinit++, text: trimmed, done: false });
+      this.newTodo = "";
+    },
+
+    completed: function(id) {
+      // this.todos = this.todos.map(td => {
+      //   if( td.id === id) {
+      //     td.done = !td.done;
+      //     console.log(td);
+      //   }
+      //   return td;
+      // })
+      console.log(id);
+      this.todos = this.todos.filter(todo => {
+        return todo.id != id;
+      });
+    }
   }
 };
 </script>
 
 <style lang="scss">
+@import url("https://fonts.googleapis.com/css?family=Pacifico|Shadows+Into+Light");
+
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  font-family: "Shadows Into Light", cursive;
+  font-size: 1.2em;
+  width: 100vw;
+  height: 100vh;
+  background-color: cadetblue;
+}
+
+.center {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+.card {
+  width: 400px;
+  min-height: 80vh;
+  border: 5px wheat;
+  border-radius: 5px;
+  background-color: bisque;
+  padding: 10px;
+  transition: all 0.3s linear;
+  filter: drop-shadow(4px 4px 4px rgba(0, 0, 0, 0.2));
+
+  .header {
+    font-family: "Pacifico", cursive;
+    color: #607d8b;
+    width: 100%;
+
+    h2 {
+      font-size: 2em;
+      text-shadow: 2px 2px rgba(0, 0, 0, 0.2);
+      margin: 15px;
+    }
+  }
+
+  .input-text {
+    box-sizing: border-box;
+    width: 100%;
+    padding: 6px;
+    background: transparent;
+    border: 0px;
+    border-bottom: 2px solid cadetblue;
+
+    :focus {
+      border: 2px cadetblue;
+    }
+  }
+
+  ul {
+    padding: 0 20px;
+  }
+  li {
+    font-size: 1.6em;
+    color: #607d8b;
+    list-style-type: none;
+    display: flex;
+    flex-direction: row;
+    padding: 5px 0px;
+    align-items: center;
+
+    p {
+      margin: 0 0 0 10px;
+    }
+  }
 }
 </style>
---
+++ src/components/todoList.vue
@@ -0,0 +1,18 @@
+<template>
+  <li>
+    <button @click="$emit('done', todo.id)">x</button>
+    <p>{{ todo.text }}</p>
+  </li>
+</template>
+
+<script>
+export default {
+  name: "todoList",
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
